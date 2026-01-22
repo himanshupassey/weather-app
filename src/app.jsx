0397aaae4b93271b84da7e11d7b5c1728033b321
Himanshu Passey <himanshupassey26@gmail.com>
2026-01-23 04:04:18 +0530

    Initial weather app commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { WeatherApp } from './components/WeatherApp'; 
+import './styles/WeatherApp.css';
+
+function App() {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
